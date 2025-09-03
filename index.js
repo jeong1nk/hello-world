6fb4700ef0113369741a44cf1c591a342adbc836
ink <kjinn121@gmail.com>
2025-09-03 23:21:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express') // 익스프레스 모듈을 가져옴
 const app = express() // 새로운 익스프레스 앱을 만듦
 const port = 5000 // 5000번 포드를 백서버로 둚
 
+const { User } = require("./models/User")
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://ink:abcd1234@test.70fbxpm.mongodb.net/?retryWrites=true&w=majority&appName=test', {
     //useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
